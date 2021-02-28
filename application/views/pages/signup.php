04c14e7c34196ca13ea5f12d14158d988deff911
Lucas Genari <lucasgenari@macadmins-MacBook-Pro.local>
2021-02-28 18:04:12 -0300

    Ajustes identação

--- application/views/pages/signup.php
+++ application/views/pages/signup.php
@@ -12,8 +12,8 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/sign-in/">
 
     <!-- Bootstrap core CSS -->
-<link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<meta name="theme-color" content="#563d7c">
+		<link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<meta name="theme-color" content="#563d7c">
 
 
     <style>
@@ -32,31 +32,31 @@
         }
       }
     </style>
-    <!-- Custom styles for this template -->
+    
     <link href="https://getbootstrap.com/docs/4.4/examples/sign-in/signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
     <form class="form-signin" method="post" action="<?= base_url() ?>signup/store">
-  <img class="mb-4" src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-	<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  		<img class="mb-4" src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+			<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 
-	<label for="inputName" class="sr-only">Name</label>
-	<input type="text" name="name" id="inputName" class="form-control" placeholder="Your Name" required autofocus>
+			<label for="inputName" class="sr-only">Name</label>
+			<input type="text" name="name" id="inputName" class="form-control" placeholder="Your Name" required autofocus>
 
-	<label for="inputCountry" class="sr-only">Country</label>
-	<input type="text" name="country" id="inputCountry" class="form-control" placeholder="Country" required autofocus>
+			<label for="inputCountry" class="sr-only">Country</label>
+			<input type="text" name="country" id="inputCountry" class="form-control" placeholder="Country" required autofocus>
+			
+			<label for="inputEmail" class="sr-only">Email address</label>
+			<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+			
+			<label for="inputPassword" class="sr-only">Password</label>
+			<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 	
-  <label for="inputEmail" class="sr-only">Email address</label>
-	<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-	
-  <label for="inputPassword" class="sr-only">Password</label>
-	<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-	
-	<p>
-		<a href="<?= base_url() ?>login">Already Have an Account?</a>
-	</p>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
-</form>
-</body>
+			<p>
+				<a href="<?= base_url() ?>login">Already Have an Account?</a>
+			</p>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			<p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+		</form>
+	</body>
 </html>
